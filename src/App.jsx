59fd8cfc61fb528a6fcf6d7736e1031ef03e084a
Jose Chim <52678389+josechim@users.noreply.github.com>
2024-06-23 14:45:44 -0600

    Add Filter by min price and category
    Add Filter funtionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
-import {products} from './mocks/products.json'
+import {products as initialProducts} from './mocks/products.json'
 import Products from './components/Products.jsx'
+import { useState } from 'react'
+import { Header } from './components/Header.jsx'
 
 export default function App(){
+    const [products,setProducts]=useState(initialProducts)
+    const [filters,setFilters]=useState(
+        {
+            category:'all',
+            minPrice:0
+        }
+    )
+    const filterProducts=(products)=>{
+        return products.filter(product=>{
+            return (
+                product.price >= filters.minPrice &&
+                (
+                    filters.category=='all' || filters.category==product.category
+                )
+
+            )
+        })
+
+    }
+
     return(
-        <Products products={products}/>
+        <>
+        <Header setFilters={setFilters}/>
+        <Products products={filterProducts(products)}/>
+        </>
+        
     )
 }
