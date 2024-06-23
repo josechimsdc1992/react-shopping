2fdf7972788b6151734126d19de40024f3638392
Jose Chim <52678389+josechim@users.noreply.github.com>
2024-06-23 15:13:52 -0600

    make a useFilters

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,18 @@ import {products as initialProducts} from './mocks/products.json'
 import Products from './components/Products.jsx'
 import { useState } from 'react'
 import { Header } from './components/Header.jsx'
+import { useFilters } from './hooks/useFilters.js'
 
 export default function App(){
     const [products,setProducts]=useState(initialProducts)
-    const [filters,setFilters]=useState(
-        {
-            category:'all',
-            minPrice:0
-        }
-    )
-    const filterProducts=(products)=>{
-        return products.filter(product=>{
-            return (
-                product.price >= filters.minPrice &&
-                (
-                    filters.category=='all' || filters.category==product.category
-                )
-
-            )
-        })
-
-    }
+    const {filterProducts,setFilters}=useFilters()
+   
+    const filteredProducts=filterProducts(products)
 
     return(
         <>
         <Header setFilters={setFilters}/>
-        <Products products={filterProducts(products)}/>
+        <Products products={filteredProducts}/>
         </>
         
     )
